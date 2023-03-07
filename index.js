168a219754bac64c9cd6d76e100a092f8fb7bd17
mrsajadpp <saju4753@gmail.com>
2023-03-07 01:50:24 -0800

    Advanced memmory

--- index.js
+++ index.js
@@ -41,36 +41,12 @@ app.get('/', (req, res) => {
 // Listen for incoming socket connections
 io.on('connection', (socket) => {
     console.log('A user connected.');
-
-    /*socket.on('message', async (data) => {
-        const openai = new OpenAIApi(configuration);
-        const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: data.content,
-            n: 1,
-            max_tokens: 2049,
-            stop: null,
-            temperature: 0.7
-        });
-        if (completion.status == 200) {
-            socket.emit(data.from, { content: completion.data.choices[0].text })
-        } else {
-            socket.emit(data.from, { content: "I don't have a specific answer please contact my team 'Dot inc'." })
-        }
-    })*/
-    // Initialize empty context object
-    let context = {};
+    const conversationHistory = {};
 
     socket.on('message', async (data) => {
-        // To lowercase
-        data.content = data.content.toLowerCase();
-
-        // Add previous conversation context to the prompt if available
-        let prompt = data.content + ', Date: ' + data.date.toLocaleString();;
-        /*if (context[data.from]) {
-            prompt = prompt + ', Date: ' + data.date.toLocaleString();
-        }*/
-        console.log(data.date.toLocaleString())
+        const content = data.content.toLowerCase();
+        const prompt = conversationHistory[data.from] ? `${conversationHistory[data.from]} ${content}` : content;
+        conversationHistory[data.from] = content;
 
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
@@ -81,25 +57,27 @@ io.on('connection', (socket) => {
             temperature: 0.7
         });
 
-        if (completion.status == 200 && completion.data.choices[0].text) {
-            // Store the current conversation context
-            //context[data.from] = prompt + '\n' + completion.data.choices[0].text;
-
-            // Send response based on previous message
-            if (data.content.split(' ').includes('hello') || data.content.split(' ').includes('hi') || data.content.split(' ').includes('hey')) {
-                socket.emit(data.from, { content: 'Hi there! How can I assist you?' });
-            } else if (data.content.split(' ').includes('bye')) {
-                socket.emit(data.from, { content: 'Goodbye! Have a nice day.' });
-            } else if (data.content.split(' ').includes('what') && data.content.split(' ').includes('your') && data.content.split(' ').includes('name')) {
-                socket.emit(data.from, { content: 'Iam dotchat powered by "Dot inc".' });
-            } else if (data.content.split(' ').includes('who') && data.content.split(' ').includes('your') && data.content.split(' ').includes('developer') || data.content.split(' ').includes('father')) {
-                socket.emit(data.from, { content: 'My father(developer) is sajad, Founder and CEO of "Dot inc".' });
-            } else {
-                socket.emit(data.from, { content: completion.data.choices[0].text });
-            }
-        } else {
+        if (completion.status !== 200 || !completion.data.choices[0].text) {
             socket.emit(data.from, { content: "Hmm something went wrong!." });
+            return;
         }
+
+        const text = completion.data.choices[0].text.toLowerCase();
+        let response;
+
+        if (content.includes('hello') || content.includes('hi') || content.includes('hey')) {
+            response = 'Hi there! How can I assist you?';
+        } else if (content.includes('bye')) {
+            response = 'Goodbye! Have a nice day.';
+        } else if (content.includes('what') && content.includes('your') && content.includes('name')) {
+            response = 'I am dotchat powered by "Dot inc".';
+        } else if (content.includes('who') && content.includes('your') && (content.includes('developer') || content.includes('father'))) {
+            response = 'My father (developer) is Sajad, Founder and CEO of "Dot inc".';
+        } else {
+            response = text;
+        }
+
+        socket.emit(data.from, { content: response });
     });
 
 
